d5387822c1a69098cc2004ad668085d1a9b3a6a4
Zhm17 <zhemo17@gmail.com>
2026-01-10 08:38:14 -0600

    - Signals triggered

--- Scripts/area3d.gd
+++ Scripts/area3d.gd
@@ -1,10 +1,15 @@
 extends Area3D
 
 
+signal area_triggered(signal_color : int)
+
+@export var area_color: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	body_entered.connect(_on_body_entered) # Replace with function body.
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,4 +19,9 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node3D) -> void:
 	print ("Body entered")
 	if body.is_class("CharacterBody3D") :
+		var home = get_node("CollisionShape3D")
+		home.set_deferred("disabled", true)
+		
 		print ("Body is Player!")
+		area_triggered.emit(area_color)
+		
--- Scripts/door.gd
+++ Scripts/door.gd
@@ -2,25 +2,42 @@ extends Node3D
 
 @onready var open_door_sfx = "res://Assets/SFX/SFX_MetalDoor.wav"
 var audio_player
+
+@export var door_color : int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	audio_player = AudioStreamPlayer.new()
-	add_child(audio_player)
-	audio_player.stream = load(open_door_sfx)
+	#var area = get_node("AreaTrigger")
+	#if  area:
+		#area.area_triggered.connect(_open)
+		#print("Area and door connected ! ")
+	#else : 
+		#print("Area not found ! ")
+		pass
 	
-	open()
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func open() -> void:
-	audio_player.play()
+func _open(signal_color : int):
+	if signal_color == door_color : 
 	
-	var tween = create_tween()
-	tween.tween_property(self, "position", Vector3(position.x, position.y - 5.0, position.z), 7.0)
-	
-	# Clean after play
-	await audio_player.finished
-	audio_player.queue_free()
+		audio_player = AudioStreamPlayer.new()
+		add_child(audio_player)
+		audio_player.stream = load(open_door_sfx)
+		audio_player.play()
+		
+		var tween = create_tween()
+		tween.tween_property(self, "position", Vector3(position.x, position.y - 5.0, position.z), 7.0)
+		
+		# Clean after play
+		await audio_player.finished
+		audio_player.queue_free()
+		
+		visible = not visible
+
+
+func _on_area_trigger_area_triggered(signal_color: int) -> void:
+	pass # Replace with function body.
