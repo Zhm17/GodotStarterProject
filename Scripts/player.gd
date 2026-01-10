28c63645f0f11b889abb2c77ada63aa606c62ad6
Zhm17 <zhemo17@gmail.com>
2026-01-10 04:29:37 -0600

    - Run animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,8 +3,13 @@ extends CharacterBody3D
 @onready var camera_mount = $camera_mount
 @onready var animation_player = $Visuals/Cha_Anims/AnimationPlayer
 
-const SPEED = 5.0
+var SPEED = 3.0
 const JUMP_VELOCITY = 4.5
+
+@export var walking_speed = 3.0
+@export var running_speed = 5.0
+
+var running = false
 
 @export var sens_horizontal = 0.5
 @export var sens_vertical = 0.5
@@ -27,6 +32,13 @@ func _input(event):
 		)
 
 func _physics_process(delta: float) -> void:
+	if(Input.is_action_pressed("run")):
+		SPEED = running_speed
+		running = true;
+	else:
+		SPEED = walking_speed
+		running = false;
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -40,8 +52,14 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if animation_player.current_animation != "Walk": 
-			animation_player.play("Walk")
+		
+		if running:
+			if animation_player.current_animation != "Run": 
+				animation_player.play("Run")
+		else :
+			if animation_player.current_animation != "Walk": 
+				animation_player.play("Walk")
+			
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
