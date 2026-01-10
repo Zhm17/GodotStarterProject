5a5d2cbcac3becac446383489ca189a7043b3049
Zhm17 <zhemo17@gmail.com>
2026-01-10 03:27:19 -0600

    - Player Animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var camera_mount = $camera_mount
+@onready var animation_player = $Visuals/Cha_Anims/AnimationPlayer
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -39,9 +40,14 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if animation_player.current_animation != "Walk": 
+			animation_player.play("Walk")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if animation_player.current_animation != "Idle": 
+			animation_player.play("Idle")
+		
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
