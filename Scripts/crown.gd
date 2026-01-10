f892ca03a51a5aee9b5ade6e7dad8c35f3fa5a44
Zhm17 <zhemo17@gmail.com>
2026-01-10 14:09:53 -0600

    - Adjustments and fixes

--- Scripts/crown.gd
+++ Scripts/crown.gd
@@ -2,15 +2,8 @@ extends Area3D
 
 signal crown_triggered(win : bool)
 
-var tween
-
 func _ready() -> void:
-	tween = get_tree().create_tween()
 	body_entered.connect(_on_body_entered)
-
-# Called when the node enters the scene tree for the first time.
-func _process(delta) -> void:
-	tween.tween_property(self, "rotation", Vector3.ZERO, 1.0)
 	
 func _on_body_entered(body: Node3D) -> void:
 	print ("Body entered")
