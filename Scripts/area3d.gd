039e858ac5889937b1b935dc8ff0e9e83f9c3480
Zhm17 <zhemo17@gmail.com>
2026-01-10 14:25:44 -0600

    - Little adjustments

--- Scripts/area3d.gd
+++ Scripts/area3d.gd
@@ -10,11 +10,11 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered) # Replace with function body.
 	
 func _on_body_entered(body: Node3D) -> void:
-	print ("Body entered")
+	#print ("Body entered")
 	if body.is_class("CharacterBody3D") :
 		var home = get_node("CollisionShape3D")
 		home.set_deferred("disabled", true)
 		
-		print ("Body is Player!")
+		#print ("Body is Player!")
 		area_triggered.emit(area_color)
 		
